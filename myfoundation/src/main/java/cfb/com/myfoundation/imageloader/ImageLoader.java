f0a5eae3495bca7363037f5bfd4d5eb5ddf5012d
caofengbin <1050430934@qq.com>
2017-02-05 20:38:28 +0800

    add code about imageLoader

---
+++ myfoundation/src/main/java/cfb/com/myfoundation/imageloader/ImageLoader.java
@@ -0,0 +1,153 @@
+package cfb.com.myfoundation.imageloader;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StatFs;
+import android.util.LruCache;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 图片加载核心类
+ * Created by fegnbincao on 2017/2/5.
+ */
+
+public class ImageLoader {
+
+    private static final String TAG = "ImageLoader";
+
+    public static final int MESSAGE_POST_RESULT = 1;
+
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+    private static final int CORE_POOL_SIZE = CPU_COUNT + 1;
+    private static final int MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1;
+    private static final long KEEP_ALIVE = 10L;
+
+    // TODO 这里的URI是什么东东！！1貌似很重要的一个变量
+
+    private static final long DISK_CACHE_SIZE = 1024 * 1024 * 50;
+    private static final int IO_BUFFER_SIZE = 8 * 1024;
+    private static final int DISK_CACHE_INDEX = 0;
+    private boolean mIsDiskLruCacheCreated = false;
+
+    // 自定义的ThreadFactory，在创建线程池的时候使用
+    // 主要用来给线程定义name，便于定位问题
+    private static final ThreadFactory sThreadFactory = new ThreadFactory() {
+
+        private final AtomicInteger mCount = new AtomicInteger(1);
+
+        @Override
+        public Thread newThread(Runnable runnable) {
+            return new Thread(runnable, "ImageLoader#" + mCount.getAndIncrement());
+        }
+    };
+
+    private static final Executor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(
+            CORE_POOL_SIZE,
+            MAXIMUM_POOL_SIZE,
+            KEEP_ALIVE,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>(),
+            sThreadFactory
+    );
+
+    // TODO 源文件70行左右，作用是处理主线程的回调问题
+
+    private Context mContext;
+    private ImageResizer mImageResizer = new ImageResizer();
+    private LruCache<String,Bitmap> mMemoryCache;
+    private DiskLruCache mDiskLruCache;
+
+    private ImageLoader(Context context) {
+        mContext = context.getApplicationContext();
+
+        // 初始化内存缓存相关
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        int cacheSize = maxMemory / 8;
+        mMemoryCache = new LruCache<String,Bitmap>(cacheSize) {
+
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+            }
+        };
+
+        // 初始化磁盘缓存相关
+        File diskCacheDir = getDiskCacheDir(mContext, "bitmap");
+        if (!diskCacheDir.exists()) {
+            diskCacheDir.mkdirs();
+        }
+        if (getUsableSpace(diskCacheDir) > DISK_CACHE_SIZE) {
+            try {
+                mDiskLruCache = DiskLruCache.open(diskCacheDir, 1, 1,
+                        DISK_CACHE_SIZE);
+                mIsDiskLruCacheCreated = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 创建全局ImageLoader单例
+     * build a new instance of ImageLoader
+     * @param context
+     * @return a new instance of ImageLoader
+     */
+    public static ImageLoader build(Context context) {
+        return new ImageLoader(context);
+    }
+
+    // 内存缓存相关操作1
+    private void addBitmapToMemoryCache(String key,Bitmap bitmap) {
+        if(getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key,bitmap);
+        }
+    }
+
+    private Bitmap getBitmapFromMemCache(String key) {
+        return mMemoryCache.get(key);
+    }
+
+    // TODO 写到了这里，明天继续啊
+
+
+
+
+
+
+
+    // 磁盘缓存文件的创建
+    public File getDiskCacheDir(Context context, String uniqueName) {
+        boolean externalStorageAvailable = Environment
+                .getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        final String cachePath;
+        if (externalStorageAvailable) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+
+        return new File(cachePath + File.separator + uniqueName);
+    }
+
+    // 磁盘缓存文件的扩容
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    private long getUsableSpace(File path) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            return path.getUsableSpace();
+        }
+        final StatFs stats = new StatFs(path.getPath());
+        return (long) stats.getBlockSize() * (long) stats.getAvailableBlocks();
+    }
+}
