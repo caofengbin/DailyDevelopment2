0a8f1e27992f4eb8317a766e6e3398d732bb1430
caofengbin <1050430934@qq.com>
2017-03-06 20:35:22 +0800

    add code about sharepreference

--- app/src/main/java/cfb/com/dailydevelopment2/example8/storedata/StoreDataActivity.java
+++ app/src/main/java/cfb/com/dailydevelopment2/example8/storedata/StoreDataActivity.java
@@ -1,9 +1,12 @@
 package cfb.com.dailydevelopment2.example8.storedata;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,23 +18,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cfb.com.dailydevelopment2.R;
 
 public class StoreDataActivity extends AppCompatActivity {
 
-    private EditText edit;
+    @BindView(R.id.edit)
+    public EditText edit;
+
+    @BindView(R.id.save_data)
+    public Button saveDataButton;
+
+    @BindView(R.id.restore_data)
+    public Button restoreDataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_data);
-        edit = (EditText) findViewById(R.id.edit);
+
+        ButterKnife.bind(this);
+
         String inputText = load();
         if(!TextUtils.isEmpty(inputText)) {
             edit.setText(inputText);
             edit.setSelection(inputText.length());
             Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
         }
+
+
+    }
+
+    @OnClick(R.id.save_data)
+    public void saveDataToSharedPreference() {
+        SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+        editor.putString("name","Tom");
+        editor.putInt("age",28);
+        editor.putBoolean("married",false);
+        editor.apply();
+        Toast.makeText(this,"保存到SharedPreference成功",Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.restore_data)
+    public void getDataToSharedPreference() {
+        SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+        String name = pref.getString("name", "");
+        int age = pref.getInt("age", 0);
+        boolean married = pref.getBoolean("married", false);
+        Log.d("MainActivity", "name is " + name);
+        Log.d("MainActivity", "age is " + age);
+        Log.d("MainActivity", "married is " + married);
+        Toast.makeText(this,"从SharedPreference读取成功",Toast.LENGTH_SHORT).show();
     }
 
     @Override
