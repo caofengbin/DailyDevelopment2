1e034d960942ad2013ee70b959624dfb9482bd84
caofengbin <1050430934@qq.com>
2017-04-01 00:16:12 +0800

    add code about percent layout

--- app/src/main/java/cfb/com/dailydevelopment2/example3/testlayout/TestLayoutActivity.java
+++ app/src/main/java/cfb/com/dailydevelopment2/example3/testlayout/TestLayoutActivity.java
@@ -1,7 +1,14 @@
 package cfb.com.dailydevelopment2.example3.testlayout;
 
 import android.os.Bundle;
+import android.support.percent.PercentLayoutHelper;
+import android.support.percent.PercentRelativeLayout;
+import android.support.v4.view.LayoutInflaterFactory;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import cfb.com.dailydevelopment2.R;
 
@@ -10,6 +17,21 @@ public class TestLayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_layout);
+        setContentView(R.layout.activity_percent_layout);
+
+        PercentRelativeLayout container = (PercentRelativeLayout) findViewById(R.id.myContainer);
+
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+
+        View myLayout = layoutInflater.inflate(R.layout.holder_layout, null);
+        PercentRelativeLayout.LayoutParams params1 = new PercentRelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT
+        );
+
+        PercentLayoutHelper.PercentLayoutInfo info = params1.getPercentLayoutInfo();
+        info.widthPercent = 0.30f;
+
+        container.addView(myLayout,params1);
+
     }
 }
